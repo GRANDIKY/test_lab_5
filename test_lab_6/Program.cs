c3776908bf2a38b07a8ab554045624928fcf19fa
GRANDIKY <mukaunitaz@gmail.com>
2023-09-15 10:38:55 +0300

    Добавьте файлы проекта.

---
+++ test_lab_6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_lab_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                result += SumDigit(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine(result);
+        }
+
+        static int SumDigit(int N)
+        {
+            int S = 0;
+            while(N > 0)
+            {
+                S += N % 10;
+                N /= 10;
+            }
+            return S;
+        }
+    }   
+}
